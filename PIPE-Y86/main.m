b7fc417161ac46390e2082754ec9ecdc29eb2d13
Xuan Yuan <txuanfdu@163.com>
2015-05-31 14:37:42 +0800

    GUI-Draw Completed.

--- PIPE-Y86/main.m
+++ PIPE-Y86/main.m
@@ -10,11 +10,17 @@
 #import "PIPE.h"
 
 int main(int argc, const char * argv[]) {
-	//return NSApplicationMain(argc, argv);
+	return NSApplicationMain(argc, argv);
+	/*
 	PIPE* testcore = [PIPE new];
 	[testcore loadImage:"/Users/xuan/Documents/计算机原理/lab3/project/asum.yo"];
-	//for (int i = 0; i < 57; i++)
+	//[testcore singleStepForward];
+	//for (int i = 0; i < 59; i++) {
 	//	[testcore singleStepForward];
+	//	[testcore singleStepBackward];
+	//}
+	[testcore setBreakpointAt:116];
 	[testcore run];
 	return 0;
+	*/
 }
